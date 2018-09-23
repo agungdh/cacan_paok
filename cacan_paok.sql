a96831eba7e230fc89af6d22555533d25d07fc5b
agungdh <agungdh@live.com>
2018-09-24 00:29:13 +0700

    update db

--- cacan_paok.sql
+++ cacan_paok.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 26, 2018 at 05:44 PM
--- Server version: 10.1.29-MariaDB-6
--- PHP Version: 5.6.36
+-- Generation Time: Sep 23, 2018 at 07:28 PM
+-- Server version: 10.1.34-MariaDB-0ubuntu0.18.04.1
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,29 +25,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `config`
---
-
-CREATE TABLE `config` (
-  `judul_aplikasi` varchar(191) NOT NULL,
-  `judul_menu` varchar(191) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `config`
---
-
-INSERT INTO `config` (`judul_aplikasi`, `judul_menu`) VALUES
-('Cacan PAOK', 'PAOK');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `kategori`
 --
 
 CREATE TABLE `kategori` (
-  `id` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `kategori` varchar(191) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -55,7 +37,7 @@ CREATE TABLE `kategori` (
 -- Dumping data for table `kategori`
 --
 
-INSERT INTO `kategori` (`id`, `kategori`) VALUES
+INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
 (1, 'Test Aja'),
 (2, 'Undangan'),
 (4, 'Gatau Gw'),
@@ -69,22 +51,26 @@ INSERT INTO `kategori` (`id`, `kategori`) VALUES
 --
 
 CREATE TABLE `surat_keluar` (
-  `id` int(11) NOT NULL,
+  `id_surat_keluar` int(11) NOT NULL,
+  `id_surat_masuk` int(11) DEFAULT NULL,
   `nosurat` varchar(191) NOT NULL,
   `tanggal` date NOT NULL,
-  `kategori_id` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `tujuan` varchar(191) NOT NULL,
   `perihal` varchar(191) NOT NULL,
-  `file` varchar(191) NOT NULL
+  `file` varchar(191) NOT NULL,
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat_keluar`
 --
 
-INSERT INTO `surat_keluar` (`id`, `nosurat`, `tanggal`, `kategori_id`, `tujuan`, `perihal`, `file`) VALUES
-(2, '1123123', '2018-07-26', 1, 'asfasf', 'asfasf asfaf 11', 'IMG_7284 (1).JPG'),
-(3, 'asf', '2018-07-26', 5, 'fas', 'fas', 'adminer-4.6.3 (3).php');
+INSERT INTO `surat_keluar` (`id_surat_keluar`, `id_surat_masuk`, `nosurat`, `tanggal`, `id_kategori`, `tujuan`, `perihal`, `file`, `id_user`) VALUES
+(2, NULL, '1123123', '2018-07-26', 1, 'asfasf', 'asfasf asfaf 11', 'IMG_7284 (1).JPG', 1),
+(3, 12, 'asf', '2018-07-26', 5, 'fas', 'fas', 'adminer-4.6.3 (3).php', 1),
+(7, NULL, '2', '2018-09-23', 6, '42', '5e', 'sftp-config.json', 1),
+(9, NULL, '21212121zxczxczx', '2018-09-23', 4, 'Analisis', 'PIECES', 'a2.docx', 1);
 
 -- --------------------------------------------------------
 
@@ -93,23 +79,23 @@ INSERT INTO `surat_keluar` (`id`, `nosurat`, `tanggal`, `kategori_id`, `tujuan`,
 --
 
 CREATE TABLE `surat_masuk` (
-  `id` int(11) NOT NULL,
+  `id_surat_masuk` int(11) NOT NULL,
   `nosurat` varchar(191) NOT NULL,
   `tanggal` date NOT NULL,
-  `kategori_id` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `pengirim` varchar(191) NOT NULL,
   `perihal` varchar(191) NOT NULL,
-  `file` varchar(191) NOT NULL
+  `file` varchar(191) NOT NULL,
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `surat_masuk`
 --
 
-INSERT INTO `surat_masuk` (`id`, `nosurat`, `tanggal`, `kategori_id`, `pengirim`, `perihal`, `file`) VALUES
-(7, '1234555', '2018-07-26', 1, 'asdasdasf', 'asfasdfsadf', 'IMG_7284.JPG'),
-(8, '111', '2018-07-26', 4, 'sicanda', 'embohhh', 'adminer-4.6.3 (1).php'),
-(9, 'asfasf', '2018-07-26', 6, 'fasf', 'asfas', 'adminer-4.6.3 (3).php');
+INSERT INTO `surat_masuk` (`id_surat_masuk`, `nosurat`, `tanggal`, `id_kategori`, `pengirim`, `perihal`, `file`, `id_user`) VALUES
+(12, '1337 3', '2018-09-23', 4, '12345', 'asdf', 'artisan', 1),
+(13, '2222', '2018-09-23', 2, 'Cacan ganteng', 'Mapping, ERD, DFD', '1930.ppt', 1);
 
 -- --------------------------------------------------------
 
@@ -118,18 +104,20 @@ INSERT INTO `surat_masuk` (`id`, `nosurat`, `tanggal`, `kategori_id`, `pengirim`
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `username` varchar(191) NOT NULL,
   `password` varchar(191) NOT NULL,
-  `nama` varchar(191) NOT NULL
+  `nama` varchar(191) NOT NULL,
+  `level` enum('a','k') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `nama`) VALUES
-(1, 'admin', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'Administrator');
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `level`) VALUES
+(1, 'admin', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'Administrator', 'a'),
+(2, 'kb', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'KABAN', 'k');
 
 --
 -- Indexes for dumped tables
@@ -139,27 +127,30 @@ INSERT INTO `user` (`id`, `username`, `password`, `nama`) VALUES
 -- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_kategori`);
 
 --
 -- Indexes for table `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `kategori_id` (`kategori_id`);
+  ADD PRIMARY KEY (`id_surat_keluar`),
+  ADD KEY `kategori_id` (`id_kategori`),
+  ADD KEY `id_surat_masuk` (`id_surat_masuk`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `kategori_id` (`kategori_id`);
+  ADD PRIMARY KEY (`id_surat_masuk`),
+  ADD KEY `kategori_id` (`id_kategori`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
@@ -170,25 +161,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_surat_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_surat_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -198,13 +189,16 @@ ALTER TABLE `user`
 -- Constraints for table `surat_keluar`
 --
 ALTER TABLE `surat_keluar`
-  ADD CONSTRAINT `surat_keluar_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`id`);
+  ADD CONSTRAINT `surat_keluar_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`),
+  ADD CONSTRAINT `surat_keluar_ibfk_2` FOREIGN KEY (`id_surat_masuk`) REFERENCES `surat_masuk` (`id_surat_masuk`),
+  ADD CONSTRAINT `surat_keluar_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 
 --
 -- Constraints for table `surat_masuk`
 --
 ALTER TABLE `surat_masuk`
-  ADD CONSTRAINT `surat_masuk_ibfk_1` FOREIGN KEY (`kategori_id`) REFERENCES `kategori` (`id`);
+  ADD CONSTRAINT `surat_masuk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`),
+  ADD CONSTRAINT `surat_masuk_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
